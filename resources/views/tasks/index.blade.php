64841dc6daaf39525247a48bf52b8cf150e49ac6
yoshinobu.fukao <yoshinobu6567@gmail.com>
2019-08-31 12:22:05 +0000

    task list change

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,11 +4,12 @@
 
     <h1>タスク一覧</h1>
     
-     @if (count($tasks) > 0)
+    @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -16,6 +17,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->title }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
